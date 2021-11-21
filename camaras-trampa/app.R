b9f252ecc3fd46c36ec89b090b9957d3702e97d2
mfvargas <mfvargas@gmail.com>
2021-11-21 08:07:52 -0600

    Agregar lectura de datos y filtro por especie

--- camaras-trampa/app.R
+++ camaras-trampa/app.R
@@ -1,49 +1,97 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Paquetes
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(hrbrthemes)
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+# Lectura de datos
+# detection
+deteccion <-
+    read.csv(
+        "https://raw.githubusercontent.com/biomonitoreo-participativo/biomonitoreo-participativo-datos/master/crtms/detection.csv"
     )
-)
 
-# Define server logic required to draw a histogram
+
+# Curación de datos
+# detection
+deteccion <-
+    deteccion %>%
+    mutate(dateTimeCaptured = as_datetime(dateTimeCaptured, format = "%Y:%m:%d %H:%M:%OS")) %>%
+    mutate(monthCaptured = month(dateTimeCaptured)) %>%
+    mutate(hourCaptured = hour(dateTimeCaptured))
+
+
+# Lista ordenada de especies + "Todas"
+lista_especies <- unique(deteccion$species)
+lista_especies <- sort(lista_especies)
+lista_especies <- c("Todas", lista_especies)
+
+
+# Definición del objeto ui
+ui <-
+    dashboardPage(
+        dashboardHeader(title = "Cámaras trampa"),
+        dashboardSidebar(sidebarMenu(
+            menuItem(
+                text = "Filtros",
+                selectInput(
+                    inputId = "especie",
+                    label = "Especie",
+                    choices = lista_especies,
+                    selected = "Todas"
+                ),
+                startExpanded = TRUE
+            )
+        )),
+        dashboardBody(fluidRow(
+            box(
+                title = "Horas de detección",
+                plotOutput(outputId = "histograma"),
+                width = 12
+            ),
+        ))
+    )
+
+# Definición de la función server
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    filtrarDatos <- reactive({
+        deteccion_filtrado <-
+            deteccion
+        
+        # Filtrado por especie
+        if (input$especie != "Todas") {
+            deteccion_filtrado <-
+                deteccion_filtrado %>%
+                filter(species == input$especie)
+        }
+        
+        return(deteccion_filtrado)
+    })
+    
+    output$histograma <- renderPlot({
+        deteccion_filtrado <-
+            filtrarDatos()
+        
+        deteccion_filtrado %>%
+            ggplot(aes(x = hourCaptured)) +
+            geom_histogram(binwidth = 1,
+                           color = "black",
+                           fill = "white") +
+            geom_density(
+                aes(y = ..count..),
+                color = "gray",
+                fill = "gray",
+                alpha = 0.4
+            ) +
+            ggtitle(input$especie) +
+            xlab("Hora") +
+            ylab("Frecuencia") +
+            theme_ipsum()
     })
 }
 
-# Run the application 
+# Ejecución de la aplicación
 shinyApp(ui = ui, server = server)
